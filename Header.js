e9a3fef9397143a764316170ace92818287591ea
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-20 21:14:51 +0400

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <MainHeader>
+     <Navlink to="/">
+      <img src="./images/logo.png" alt="my" />
+     </NavLink>
+     <Nav />
+     </MainHeader>
+    );
+};
+
+const MainHeader
